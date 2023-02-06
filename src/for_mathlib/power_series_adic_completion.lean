2d99df3fb617977f834c9933439b50888abc7e35
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2023-02-06 19:16:15 +0100

    wip

--- src/for_mathlib/power_series_adic_completion.lean
+++ src/for_mathlib/power_series_adic_completion.lean
@@ -345,19 +345,24 @@ begin
     refl,
 end
 
+
+-- do for general hahn series
+lemma fae_order_neg {f : ratfunc K} : -- (hf : f ≠ 0) :
+ (- f : laurent_series K).order = (f : laurent_series K).order := sorry
+
 lemma fae_order_eq_val' (f : ratfunc K) (hf : f ≠ 0) :
  ↑(multiplicative.of_add ((f : laurent_series K).order)) = ((ideal_X K).valuation f)⁻¹ :=
 begin
   rw [← neg_neg f],
-    sorry,
-  -- have := (ratfunc.coe_alg_hom K).map_neg,
-  -- rw of_add_neg,
+  rw ratfunc.coe_def,
+  rw (ratfunc.coe_alg_hom K).map_neg,
+  rw ← ratfunc.coe_def,
+  rw fae_order_neg,
+  have := fae_order_eq_val K (-f) _,
+  rw of_add_neg at this,
+  --a parte qualche scemenza di invertire cose in ℤₘ₀, ci siamo
+  rw inv_eq_iff_inv_eq at this,
 end
-
--- example (G : Type*) [group G] [has_lt G] (a b : G) : a < b ↔ b⁻¹ < a⁻¹ :=
--- begin
---   rw inv_lt',
--- end
 
 lemma coeff_fae (d : ℤ) (x y : ratfunc K) (H : (x, y) ∈ (set_fae K d)) :
  (x : laurent_series K).coeff d = (y : laurent_series K).coeff d :=
