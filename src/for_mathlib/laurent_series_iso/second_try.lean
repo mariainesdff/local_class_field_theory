85545e9166390782be25a0ee84965d14909df614
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2023-05-26 01:02:12 +0200

    many new sorry's

--- src/for_mathlib/laurent_series_iso/second_try.lean
+++ src/for_mathlib/laurent_series_iso/second_try.lean
@@ -13,9 +13,27 @@ instance : separated_space (laurent_series K) := sorry
 
 instance : complete_space (laurent_series K) := sorry
 
+instance : topological_ring (laurent_series K) := sorry
+
+instance : uniform_add_group (laurent_series K) := sorry
+
 noncomputable!
 def coe_is_inducing : uniform_inducing (coe : (ratfunc K) → (laurent_series K)) :=
 { comap_uniformity := sorry }
+
+lemma unif_cont_coe : uniform_continuous (coe : (ratfunc K) → (laurent_series K)) := sorry
+
+noncomputable!
+def coe_hom : (ratfunc K) →+* (laurent_series K) :=
+{ to_fun := coe,
+  map_one' := sorry,
+  map_mul' := sorry,
+  map_zero' := sorry,
+  map_add' := sorry }
+
+lemma coe_hom_cont : continuous (coe_hom K) := sorry
+
+def ϕ := uniform_space.completion.extension_hom (coe_hom K)
 
 lemma coe_range_dense : dense_range (coe : (ratfunc K) → (laurent_series K)) := sorry
 
@@ -32,30 +50,18 @@ def laurent_series_pkg : abstract_completion (ratfunc K) :=
   uniform_inducing := coe_is_inducing K,
   dense := coe_range_dense K}
 
--- noncomputable!
 def ψ : (completion_of_ratfunc K) ≃ᵤ laurent_series K :=
   abstract_completion.compare_equiv (ratfunc_pkg K) (laurent_series_pkg K)
 
--- def ϕ : (completion_of_ratfunc K) → laurent_series K :=
-#check uniform_space.completion.extension (coe : (ratfunc K) → (laurent_series K))
+lemma unif_cnt_ψ : uniform_continuous (ψ K) := sorry
 
-section sub
+lemma aux (f : ratfunc K) : (f : laurent_series K) = ψ K ↑f := sorry
 
-lemma has_sub_hat : @has_neg.neg (completion_of_ratfunc K) _ =
-  uniform_space.completion.map (@has_neg.neg (ratfunc K) _) :=
-begin
-  sorry,
-end
+lemma ϕ_eq_ψ : (ϕ K (coe_hom_cont K)).to_fun = (ψ K).to_fun :=
+  uniform_space.completion.extension_unique (unif_cont_coe K) (unif_cnt_ψ K) (aux K)
 
-lemma at_least_for_neg (f : completion_of_ratfunc K) : (ψ K) (-f) = - (ψ K) f :=
-begin
-  suffices : (ψ K) ∘ has_neg.neg = has_neg.neg ∘ (ψ K),
-  exact congr_fun this f,
-  rw has_sub_hat,
-  rw ψ,
-  sorry,
-end
-
-
-end sub
-
+noncomputable! def laurent_series.ring_equiv' : 
+  ring_equiv (completion_of_ratfunc K) (laurent_series K) :=
+{ map_mul' := (ϕ K (coe_hom_cont K)).map_mul',
+  map_add' := (ϕ K (coe_hom_cont K)).map_add',
+  .. ψ K }
