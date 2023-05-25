7a57dd691e5694735ccff3f1cab655ed348dd046
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2023-05-25 23:02:49 +0200

    something on sub

--- src/for_mathlib/laurent_series_iso/second_try.lean
+++ src/for_mathlib/laurent_series_iso/second_try.lean
@@ -1,31 +1,56 @@
-import for_mathlib.laurent_series_iso.power_series_adic_completion
+import for_mathlib.power_series_adic_completion
 import topology.uniform_space.abstract_completion
 
 noncomputable theory
 
 variables (K : Type*) [field K]
 
-open uniform_space
+open uniform_space ratfunc
 
--- instance : uniform_space (laurent_series K) := sorry
+instance : uniform_space (laurent_series K) := sorry
 
--- instance : complete_space (laurent_series K) := sorry
+instance : separated_space (laurent_series K) := sorry
 
--- example : uniform_space (ratfunc K) := infer_instance
+instance : complete_space (laurent_series K) := sorry
 
-noncomputable def laurent_series_as_pkg : abstract_completion (ratfunc K) :=
+noncomputable!
+def coe_is_inducing : uniform_inducing (coe : (ratfunc K) → (laurent_series K)) :=
+{ comap_uniformity := sorry }
+
+lemma coe_range_dense : dense_range (coe : (ratfunc K) → (laurent_series K)) := sorry
+
+noncomputable!
+def ratfunc_pkg : abstract_completion (ratfunc K) := uniform_space.completion.cpkg 
+
+noncomputable!
+def laurent_series_pkg : abstract_completion (ratfunc K) :=
+{ space := laurent_series K,
+  coe := coe,
+  uniform_struct := infer_instance,
+  complete := infer_instance,
+  separation := infer_instance,
+  uniform_inducing := coe_is_inducing K,
+  dense := coe_range_dense K}
+
+-- noncomputable!
+def ψ : (completion_of_ratfunc K) ≃ᵤ laurent_series K :=
+  abstract_completion.compare_equiv (ratfunc_pkg K) (laurent_series_pkg K)
+
+section sub
+
+lemma has_sub_hat : @has_neg.neg (completion_of_ratfunc K) _ =
+  uniform_space.completion.map (@has_neg.neg (ratfunc K) _) :=
 begin
   sorry,
 end
 
--- def φ : (ratfunc K) → (laurent_series K) := sorry
+lemma at_least_for_neg (f : completion_of_ratfunc K) : (ψ K) (-f) = - (ψ K) f :=
+begin
+  suffices : (ψ K) ∘ has_neg.neg = has_neg.neg ∘ (ψ K),
+  exact congr_fun this f,
+  rw has_sub_hat,
+end
 
--- lemma uniform_continuous_φ : uniform_continuous (φ K):= sorry
 
--- def ψ₀ := uniform_space.completion.extension (φ K)
+end sub
 
--- def ψ : (completion_of_ratfunc K) ≃ laurent_series K :=
--- -- def ψ : (completion_of_ratfunc K) ≃ᵤ laurent_series K :=
--- begin
---   have : 0 = 0,
--- end 
