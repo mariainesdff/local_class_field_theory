ba0933f7094b3f92a1ed1d709a50478cce2e9111
Maria Ines de Frutos Fernandez <mariaines.dff@gmail.com>
2023-06-07 14:31:37 +0200

    lint residue_ring_def

--- src/for_mathlib/residue_ring_def.lean
+++ src/for_mathlib/residue_ring_def.lean
@@ -10,15 +10,16 @@ variable [val : valued K Γ₀]
 include val
 
 def open_ball_as_ideal : ideal (valuation.integer (val.v)) :=
-{ carrier := {x : valued.v.integer | (@valued.v K _ _ _ _) ↑x < (1 : Γ₀)},
-  add_mem' := sorry,
+{ carrier   := {x : valued.v.integer | (@valued.v K _ _ _ _) ↑x < (1 : Γ₀)},
+  add_mem'  := sorry,
   zero_mem' := sorry,
   smul_mem' := sorry }
   
 def residue_ring : Type* := (valuation.integer (val.v)) ⧸ (open_ball_as_ideal K)
 
+instance : inhabited (valuation.residue_ring K) := ⟨ideal.quotient.mk _ 0⟩
+
 instance : comm_ring (residue_ring K) := ideal.quotient.comm_ring _
 
 end valuation
-
 
