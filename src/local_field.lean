c75797531ad7fc3ca152b4074b04868a60039aa5
Maria Ines de Frutos Fernandez <mariaines.dff@gmail.com>
2023-06-07 16:44:05 +0200

    expand local field def

--- src/local_field.lean
+++ src/local_field.lean
@@ -1,4 +1,4 @@
-import eq_characteristic.basic
+import eq_characteristic.valuation
 import for_mathlib.residue_ring_def
 import mixed_characteristic.valuation
 import from_mathlib.rank_one_valuation
@@ -9,26 +9,28 @@ open_locale discrete_valuation
 
 class local_field (K : Type*) [field K] extends valued K ℤₘ₀ :=
 (complete : complete_space K)
--- (is_discrete : is_discrete (@valued.v K _ ℤₘ₀ _ _))
+(is_discrete : is_discrete (@valued.v K _ ℤₘ₀ _ _))
 (finite_residue_field : fintype (residue_ring K))
 
 namespace mixed_char_local_field
 
-@[priority 100]
-instance (p : out_param ℕ) [fact(nat.prime p)] (K : Type*) [field K] [mixed_char_local_field p K] :
-  local_field K := sorry
-
---#check mixed_char_local_field.local_field
+@[priority 100] noncomputable instance (p : out_param ℕ) [fact(nat.prime p)] (K : Type*) [field K] 
+  [mixed_char_local_field p K] : local_field K := 
+{ complete             := sorry,
+  is_discrete          := sorry,
+  finite_residue_field := sorry,
+  ..(mixed_char_local_field.with_zero.valued K) }
 
 end mixed_char_local_field
 
 namespace eq_char_local_field
 
-@[priority 100]
-instance (p : out_param ℕ) [fact(nat.prime p)] (K : Type*) [field K] [eq_char_local_field p K] :
-  local_field K := sorry
-
---#check eq_char_local_field.local_field
+@[priority 100] noncomputable instance (p : out_param ℕ) [fact(nat.prime p)] (K : Type*) [field K]
+  [eq_char_local_field p K] : local_field K := 
+{ complete             := sorry,
+  is_discrete          := sorry,
+  finite_residue_field := sorry,
+  ..(eq_char_local_field.with_zero.valued K) }
 
 end eq_char_local_field
 
