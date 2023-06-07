c3176b63c0cf722dd70a991759e510535a81ec66
Maria Ines de Frutos Fernandez <mariaines.dff@gmail.com>
2023-06-07 11:34:32 +0200

    minimize imports in with_zero.lean

--- src/with_zero.lean
+++ src/with_zero.lean
@@ -3,7 +3,10 @@ Copyright (c) 2023 María Inés de Frutos-Fernández, Filippo A. E. Nuccio. All 
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: María Inés de Frutos-Fernández, Filippo A. E. Nuccio
 -/
-import discrete_valuation_ring.basic
+
+import ring_theory.valuation.basic
+import data.real.nnreal
+
 
 noncomputable theory
 
