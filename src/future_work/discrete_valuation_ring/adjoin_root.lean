2a40b30aba5483f9e48af0cdd9aad78b9645f2da
Maria Ines de Frutos Fernandez <mariaines.dff@gmail.com>
2023-06-21 13:49:39 +0200

    remove future work from dvr.extensions

---
+++ src/future_work/discrete_valuation_ring/adjoin_root.lean
@@ -0,0 +1,55 @@
+import discrete_valuation_ring.extensions
+
+namespace discrete_valuation_ring
+
+variables {A : Type*} [comm_ring A] [is_domain A] [discrete_valuation_ring A]
+
+namespace adjoin_root
+
+lemma is_domain_of_irreducible {f : polynomial A} 
+  (hf : irreducible (polynomial.map (algebra_map A (local_ring.residue_field A)) f)) :
+  is_domain (adjoin_root f) :=
+sorry
+
+-- Ch. I, Section 6, Prop. 15 of Serre's "Local Fields"
+lemma discrete_valuation_ring_of_irreducible {f : polynomial A} 
+  (hf : irreducible (polynomial.map (algebra_map A (local_ring.residue_field A)) f)) :
+  @discrete_valuation_ring (adjoin_root f) _ (is_domain_of_irreducible hf) :=
+sorry
+
+-- Ch. I, Section 6, Cor. 1 of Serre's "Local Fields"
+lemma is_integral_closure_of_irreducible {f : polynomial A} 
+  (hf : irreducible (polynomial.map (algebra_map A (local_ring.residue_field A)) f)) :
+  is_integral_closure (adjoin_root f) A (fraction_ring (adjoin_root f)) :=
+sorry
+
+end adjoin_root
+
+local notation `K` := fraction_ring A
+
+variables (L : Type*) [field L] [algebra K L] [finite_dimensional K L] [algebra A L]
+[is_scalar_tower A K L]
+
+open finite_dimensional
+
+-- Serre's Proposition 16 in Chapter I, Section 6: we may want the algebra instance to become an
+-- explicit variable so that when we use the definition we do not need `@`.
+noncomputable!
+def integral_closure_equiv_algebra_adjoin
+  (hB : discrete_valuation_ring (integral_closure A L))
+  [algebra (local_ring.residue_field A) (@local_ring.residue_field _ _ hB.to_local_ring)]
+  (hpb : power_basis (local_ring.residue_field A) (@local_ring.residue_field _ _ hB.to_local_ring))
+  (hdeg : finrank K L = hpb.dim) (x : (integral_closure A L))
+  (hx : ideal.quotient.mk (@local_ring.maximal_ideal _ _ hB.to_local_ring) x = hpb.gen) : 
+  (integral_closure A L) ≃+* algebra.adjoin A ({x} : set (integral_closure A L)) := sorry
+
+noncomputable!
+def integral_closure_equiv_adjoin_root
+  (hB : discrete_valuation_ring (integral_closure A L))
+  [algebra (local_ring.residue_field A) (@local_ring.residue_field _ _ hB.to_local_ring)]
+  (hpb : power_basis (local_ring.residue_field A) (@local_ring.residue_field _ _ hB.to_local_ring))
+  (hdeg : finrank K L = hpb.dim) (x : (integral_closure A L))
+  (hx : ideal.quotient.mk (@local_ring.maximal_ideal _ _ hB.to_local_ring) x = hpb.gen) : 
+  (integral_closure A L) ≃+* adjoin_root (minpoly A x) := sorry
+
+end discrete_valuation_ring
---
+++ src/future_work/discrete_valuation_ring/ramification.lean
@@ -0,0 +1,19 @@
+import discrete_valuation_ring.extensions
+
+namespace discrete_valuation_ring
+
+open valuation
+
+variables {A : Type*} [comm_ring A] [is_domain A] [discrete_valuation_ring A]
+-- We need to indicate in the doctring that h_alg is not an instance so when we apply it 
+-- with local fields...
+variables {B : Type*} [comm_ring B] [is_domain B] [discrete_valuation_ring B] (h_alg : algebra A B)
+
+local notation `e(` B`,`A`)` := ideal.ramification_idx h_alg.to_ring_hom
+  (local_ring.maximal_ideal A) (local_ring.maximal_ideal B)
+
+lemma uniformizer_iff_unramified {a : A} (ha : is_uniformizer valued.v (↑a : fraction_ring A)) :
+  is_uniformizer valued.v (↑(h_alg.to_ring_hom a) : fraction_ring B) ↔ e(B,A) = 1 :=
+sorry
+
+end discrete_valuation_ring
